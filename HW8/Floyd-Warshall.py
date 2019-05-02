4025d787663e928d5803fdc4f0d29784f30548c4
JonahTuckman <jtuckman9@gmail.com>
2019-05-02 16:51:52 -0400

    FLoyd Warshal D done

---
+++ HW8/Floyd-Warshall.py
@@ -0,0 +1,89 @@
+#### FLOYD'S ALGORITHM ####
+
+## All pairs shortest path problem
+## Find shortest distance between every pair of vertices
+
+### Infinity is huge number
+INFINITY = 999999999999
+
+##### INPUT #####
+D = [ [0, 2, INFINITY, 1, 8],
+        [6, 0, 3, 2, INFINITY],
+        [INFINITY, INFINITY, 0, 4, INFINITY],
+        [INFINITY, INFINITY, 2, 0, 3],
+        [3, INFINITY, INFINITY, INFINITY, 0]]
+
+TextBookTest = [
+        [0, 3, 8, INFINITY, -4],
+        [INFINITY, 0, INFINITY, 1, 7],
+        [INFINITY, 4, 0, INFINITY, INFINITY],
+        [2, INFINITY, -5, 0, INFINITY],
+        [INFINITY, INFINITY, INFINITY, 6, 0]
+]
+
+
+## 5 vertices
+vertices = 5
+
+## Function to print D()
+def FloydsDAlgo(diagram):
+    # 2D distance array is return value
+    ## Initial min distance for each vertice
+    ## Finds distance between i and j
+
+                                                            ## vertice in distance array
+    ## Loop through entirity of the diagram with 5 vertices
+    for k in range(vertices):
+        # iterate through and allow each vertice to be the source of the distance calculation
+        for i in range(vertices):
+            # iterate through and allow each vertice to be the destination of the i source vertices
+            for j in range(vertices):
+                # check for min distance, update if lower than min
+                if((diagram[i][k] + diagram[k][j]) < diagram[i][j]):
+                    diagram[i][j] = (diagram[i][k] + diagram[k][j])
+        print("D(%7s)" % k)
+        printDiagram(diagram)
+    # When exited final iteration, print the distance
+    #printDiagram(distance)
+## Function to print pi()
+def FloydsPiAlgo(diagram):
+    ## Initialize Pi matrix
+    for k in range(vertices):
+        for i in range(vertices):
+            for j in range(vertices):
+                if(k == 0):
+                    if((i == j) or (diagram[i][j] == INFINITY)):
+                        diagram[i][j] = None
+                    else:
+                        diagram[i][j] = i
+                else:
+                    if((diagram[i][j] < diagram[i][k] + diagram[k][j])):
+                        diagram[i][j] = diagram[i][j]
+                    else:
+                        diagram[i][j] = diagram[k][j]
+        print("Pi(%7s)" % k)
+        printDiagram(diagram)
+
+
+
+
+## Helper function to print the solution distance
+def printDiagram(distance):
+    # create nested foor loops for 2D array
+    for i in range(vertices):
+        print("[", end = "")
+        for j in range(vertices):
+            # Check for infinite values
+            if (distance[i][j] == INFINITY):
+                print("%7s" % ("INF "), end = "")
+            # If value is not infinite
+            else:
+                print("%7s" % ( distance[i][j]), end = "")
+            if (j == vertices - 1):
+                print("]")
+
+##FloydsDAlgo(TextBookTest)
+## D algo confirmed working
+FloydsDAlgo(D)
+FloydsPiAlgo(TextBookTest)
+##FloydsPiAlgo(D)
