aa0b1157240346a1a51242894b4ea35f2c6a328c
JonahTuckman <jtuckman9@gmail.com>
2019-05-06 14:13:44 -0400

    book input in matrix mult

---
+++ HW8/Floyd-Warshall.py
@@ -0,0 +1,89 @@
+#### FLOYD'S ALGORITHM ####
+
+## All pairs shortest path problem
+## Find shortest distance between every pair of vertices
+
+import numpy as np
+
+### Infinity is huge number
+INFINITY = 999999999999
+
+
+
+## 5 vertices
+vertices = 5
+
+
+## Function to print D()
+def FloydsAlgo(diagram):
+    # 2D distance array is return value
+    ## Initial min distance for each vertice
+    ## Finds distance between i and j
+    pi = np.zeros((vertices, vertices))
+    for i in range(vertices):
+        for j in range(vertices):
+            if((i == j) or (diagram[i][j] == INFINITY)):
+                pi[i][j] = None
+            else:
+                pi[i][j] = i
+                                                                            ## vertice in distance array
+    ## Loop through entirity of the diagram with 5 vertices
+    for k in range(vertices):
+        # iterate through and allow each vertice to be the source of the distance calculation
+        for i in range(vertices):
+            # iterate through and allow each vertice to be the destination of the i source vertices
+            for j in range(vertices):
+                # check for min distance, update if lower than min
+                if((diagram[i][k] + diagram[k][j]) < diagram[i][j]):
+                    diagram[i][j] = (diagram[i][k] + diagram[k][j])
+                    pi[i][j] = pi[k][j]
+
+        print("D(%d)" % k)
+        printDiagram(diagram)
+        print("Pi(%d)" % k)
+        printDiagram(pi)
+    # When exited final iteration, print the distance
+    #printDiagram(distance)
+
+
+
+## Helper function to print the solution distance
+def printDiagram(distance):
+    # create nested foor loops for 2D array
+    for i in range(vertices):
+        print("[", end = "")
+        for j in range(vertices):
+            # Check for infinite values
+            if (distance[i][j] == INFINITY):
+                print("%7s" % ("INF"), end = "")
+            # If value is not infinite
+            else:
+                print("%7s" % ( distance[i][j]), end = "")
+            if (j == vertices - 1):
+                print("]")
+
+def main():
+
+    ##### INPUT #####
+    D = [ [0, 2, INFINITY, 1, 8],
+            [6, 0, 3, 2, INFINITY],
+            [INFINITY, INFINITY, 0, 4, INFINITY],
+            [INFINITY, INFINITY, 2, 0, 3],
+            [3, INFINITY, INFINITY, INFINITY, 0]]
+
+    ##### Example input from textbook used to check against their output ####
+    TextBookTest = [
+            [0, 3, 8, INFINITY, -4],
+            [INFINITY, 0, INFINITY, 1, 7],
+            [INFINITY, 4, 0, INFINITY, INFINITY],
+            [2, INFINITY, -5, 0, INFINITY],
+            [INFINITY, INFINITY, INFINITY, 6, 0]
+    ]
+
+## Algo confirmed working
+    #FloydsAlgo(TextBookTest)
+## Input function call
+    FloydsAlgo(D)
+
+if __name__ == "__main__":
+    main()
---
+++ HW8/Matrix-Chain-Order.py
@@ -0,0 +1,79 @@
+### Matrix-Chain-Order
+#import numpy as np
+#from numpy import array
+
+INFINITY = 999999999999
+
+
+def MatrixChainOrder(p):
+    n = len(p) - 1
+
+    # m is minimum number of multiplications needed
+    m = [[0 for x in range(0,n)] for y in range(0,n)]
+    # matrix after minimum multiplication split
+    s = [[0 for x in range(0,n)] for y in range(0,n)]
+
+
+    ## Make the diagonal 0
+    for i in range(1,n):
+        m[i][i] = 0
+
+    # Find the shortest matrix cost
+    for l in range(1, n):
+        for i in range(0, n - l):
+            j = i + l
+            # Set value to large infinity value set above
+            m[i][j] = INFINITY
+            for k in range(i, j):
+                # Calculate the cost of each iteration
+                q = m[i][k] + m[k+1][j] + p[i]*p[k+1]*p[j+1]
+                # find min cost
+                if (q < m[i][j]):
+                    # if min cost:
+                    # set m value to the cost
+                    m[i][j] = q
+                    # set s value to the iteration number
+                    s[i][j] = k
+
+    return m, s
+
+def Print_Optimal_Parens(s, i, j):
+    if i == j:
+        # Print the A value
+        print("A_%d" % i, end = "")
+    else:
+        # print parenthasis and recursively call
+        print("(", end = "")
+        Print_Optimal_Parens(s, i, s[i][j])
+        Print_Optimal_Parens(s, s[i][j] + 1, j)
+        # once call has concluded and returned we add closing parenthasis
+        print(")", end = "")
+
+
+def main():
+
+    ## Input:
+    p = [40, 20, 30, 10, 30]
+    book = [30, 35, 15, 5, 10, 20, 25]
+
+    # create m and s matrix with input
+    m, s =  MatrixChainOrder(book)
+
+    # Printing M and S matrix
+    print("M Matrix")
+    for i in m:
+        print(i)
+    print("")
+    print("S Matrix")
+    for i in s:
+        print(i)
+    print("")
+    # Parenthasis using m and s
+    Print_Optimal_Parens(s,0,5)
+    print("")
+    print("")
+
+
+
+if __name__ == "__main__":
+    main()
